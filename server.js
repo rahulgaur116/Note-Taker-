88092440987320f9e1d907e40b77104688d74543
Rahul Sharma <rahulgaur116@gmail.com>
2024-05-09 20:31:08 -0400

    updated server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 
 // asks express to create a route for every file in the 'public' folder and give it a '/' route
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // sets up express app to handel data parser, middle wear created req.body
 app.use(express.urlencoded({ extended: true }));
